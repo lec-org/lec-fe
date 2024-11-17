edd4e239fb42ad206295971be0cff60099e17b99
apries <17311373689@163.com>
2024-11-18 00:35:02 +0800

    添加 Modal 组件

--- apps/lech/src/components/search/Search.tsx
+++ apps/lech/src/components/search/Search.tsx
@@ -2,15 +2,15 @@ import { useState } from 'react'
 import styles from './search.module.scss'
 
 export default function Search() {
-  const [isActive, setIsActive] = useState(false) 
+  const [isActive, setIsActive] = useState(false)
   return (
-    <form role="search" className={[styles['search-form'], isActive? styles['active'] : ''].join(' ')}>
-      <input 
-        type='search' 
-        className={[styles['search-input'], isActive? styles['active'] : ''].join(' ')} 
-        maxLength={64} 
+    <form role="search" className={[styles['search-form'], isActive ? styles['active'] : ''].join(' ')}>
+      <input
+        type='search'
+        className={[styles['search-input'], isActive ? styles['active'] : ''].join(' ')}
+        maxLength={64}
         placeholder='十里淘金'
-        onFocus={()=>{setIsActive(true)}}
+        onFocus={() => { setIsActive(true) }}
       />
       <div className={styles['search-icon-container']}>
         <div className={styles['icon']} />
--- apps/lech/src/pages/home/frontend/Frontend.tsx
+++ apps/lech/src/pages/home/frontend/Frontend.tsx
@@ -1,5 +1,17 @@
+import { Modal } from "@/components"
+import { useState } from "react"
+
 export default function Frontend() {
+  const [isShowModal, setIsShowModal] = useState(false)
   return (
-    <div>前端</div>
+    <>
+      <div onClick={() => setIsShowModal(true)}>前端</div>
+      <Modal
+        isOpen={isShowModal}
+        onClose={() => setIsShowModal(false)}
+      >
+        <div>前端</div>
+      </Modal>
+    </>
   )
 }
